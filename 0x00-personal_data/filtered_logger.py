8a9be4fc91bc8a300cbb3ca49b7e727b726da3b1
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-01 10:18:27 -0700

    Tasks 1, 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,10 +28,10 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
         original_message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
