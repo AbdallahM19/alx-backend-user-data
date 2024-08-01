9ebf3e07f92bb05a0703102424bc005259ac2c43
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-01 05:48:13 -0700

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-        message: str, separator: str) -> str:
+                 message: str, separator: str) -> str:
     """filter datum function"""
     pattern = r'({})=([^{}]*)'.format('|'.join(fields), separator)
     return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}', message)
