776255452f321df44f952d4b915798d41ab4ffde
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-01 05:42:47 -0700

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 """Personal Data"""
 
-import re
 from typing import List
+import re
+
+
+patterns = {
+    "extract": lambda field, message, separator: re.search(
+        r'{}=([^{}]*)'.format(field, separator), message
+    ).group(0)
+}
 
 
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str
         ) -> str:
     """filter datum function"""
-    return re.sub(
-        r'({})=([^{}]*)'.format('|'.join(fields), separator),
-        lambda m: f'{m.group(1)}={redaction}', message)
+    for field in fields:
+        regex_ing = patterns['extract'](field, message, separator)
+        message = re.sub(regex_ing, "{}={}".format(field, redaction), message)
+    return message
