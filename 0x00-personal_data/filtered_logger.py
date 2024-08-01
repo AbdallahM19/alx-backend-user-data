3c3ba84a547aab020e5d4fe5e8d3184cab303dfe
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-01 10:16:48 -0700

    Tasks 1, 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,11 +2,44 @@
 """Personal Data"""
 
 from typing import List
+import logging
 import re
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
     """filter datum function"""
     pattern = r'({})=([^{}]*)'.format('|'.join(fields), separator)
     return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Creates and returns a logger configured for user data."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
