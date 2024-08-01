571e529852ff878ef48b81cb5498878baeea1f28
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-01 10:24:32 -0700

    Tasks 1, 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
 """Personal Data"""
 
-from typing import List
-import logging
 import re
+import logging
+from typing import List
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str
-        ) -> str:
-    """filter datum function"""
-    pattern = r'({})=([^{}]*)'.format('|'.join(fields), separator)
-    return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}', message)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -29,9 +21,17 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        original_message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            original_message, self.SEPARATOR)
+        msg = super(RedactingFormatter, self).format(record)
+        test = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return test
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
+    """filter datum function"""
+    pattern = r'({})=([^{}]*)'.format('|'.join(fields), separator)
+    return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}', message)
 
 
 def get_logger() -> logging.Logger:
