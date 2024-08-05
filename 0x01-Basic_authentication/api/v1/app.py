269cc19e9725f28eb5bff4e871ad763da495fe0c
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-06 01:26:57 +0200

    Task 2, 3

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,10 +22,15 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def Unauthorized(error) -> str:
+def unauthorized(error) -> str:
     """Not found handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
