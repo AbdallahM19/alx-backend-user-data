c2264a38582a8a3f49e8384e1f67c251a8b35a28
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-05 16:35:07 -0700

    Task 2, 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,12 +10,10 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if a path requires auth"""
         return False
-    
 
     def authorization_header(self, request=None) -> str:
         """Get the auth header from the request"""
         return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Get the current user from the request"""
