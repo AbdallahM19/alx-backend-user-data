90d31d0d8328fbe6f2b98f5bdecbbb894428d836
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-08 23:27:17 +0200

    Session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """a class SessionAuth"""
 
+from uuid import uuid4
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """SessionAuth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create_session method"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
