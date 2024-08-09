e282c4353bee0d40fbb360b902cdea1a6cbf10e2
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-09 03:07:49 +0200

    Session authentication

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -11,23 +11,25 @@ class SessionDBAuth(SessionExpAuth):
     def create_session(self, user_id=None) -> str:
         """Create a session and store it in the database"""
         session_id = super().create_session(user_id)
-        if session_id is None:
+        if session_id is None or type(session_id) != str:
             return None
-        user_session = UserSession(user_id=user_id, session_id=session_id)
+        kwargs = {
+            'user_id': user_id,
+            'session_id': session_id,
+        }
+        user_session = UserSession(**kwargs)
         user_session.save()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """Retrieve the User ID from the database by session_id"""
-        if session_id is None:
-            return None
         try:
             user_sessions = UserSession.search({'session_id': session_id})
         except Exception:
             return None
         user_session = user_sessions[0]
-        if self.session_duration <= 0:
-            return user_session.user_id
+        if self.user_sessions <= 0:
+            return None
         if user_session.created_at + timedelta(
             seconds=self.session_duration
         ) < datetime.now():
@@ -39,13 +41,10 @@ class SessionDBAuth(SessionExpAuth):
         Destroy the session based on the session_id
         from the request cookie
         """
-        if request is None:
-            return False
         session_id = self.session_cookie(request)
-        if session_id is None:
-            return False
-        user_sessions = UserSession.search({'session_id': session_id})
-        if not user_sessions:
+        try:
+            user_sessions = UserSession.search({'session_id': session_id})
+        except Exception:
             return False
         if len(user_sessions) <= 0:
             return False
