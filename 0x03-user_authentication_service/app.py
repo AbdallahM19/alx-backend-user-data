c715360887a2459fd4ccf208ccebfc526d468fc1
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-15 22:14:44 +0200

    Tasks

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,7 +42,7 @@ def login() -> str:
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> str:
     """Logout a user"""
-    session_id = request.form.get("session_id")
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
